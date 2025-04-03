1f4c9668a79478df283107f8df6e1826d3db1327
“tntalus9” <tntalus@proton.me>
2025-04-04 03:07:44 +0530

    Removed the testing code used to verify that kitchen objects could be moved between different parents

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -5,22 +5,8 @@ public class ClearCounter : MonoBehaviour, IKitchenObjectParent
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
-    [SerializeField] private ClearCounter secondClearCounter;
     
-    [SerializeField]private bool testing = true;
     KitchenObject kitchenObject;
-
-    private void Update()
-    {
-        if (testing && Input.GetKeyDown(KeyCode.T))
-        {
-            if (kitchenObject != null)
-            {
-                kitchenObject.SetKitchenObjectParent(secondClearCounter);
-            }
-        }
-
-    }
 
     public void Interact(Player player)
     {
